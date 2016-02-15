259e2c5d29ae1c9bda645db30a9d3190e67ed9ed
recklessmoxie <shellybarkley@ymail.com>
2016-02-15 09:03:44 -0800

    Created problem-1 content.

---
+++ app.js
@@ -0,0 +1,20 @@
+// //You'll need one of these for each answer
+// var pTagOne = document.getElementById('ans-one');
+// var ans1 = prompt('Did Sam live in South Dakota for 8 years?');
+// if (ans1.toLowerCase() === 'n' || ans1.toUpperCase() === 'NO') {
+//   // alert('You\'re right! Sam never lived in South Dakota!');
+//   // The alert() is replaced with the following line
+//   pTagOne.textContent = 'You\'re right! Sam never lived in South Dakota!';
+// } else if (ans1.toLowerCase() === 'y' || ans1.toUpperCase() === 'YES') {
+//   // alert('Nope! Sam never lived in South Dakota!');
+//   // The alert() is replaced with the following line
+//   pTagOne.textContent = 'Nope! Sam never lived in South Dakota!';
+// } else {
+//   // This error-correction can stay as an alert
+//   alert('Please give a yes or no answer.');
+// }
+
+
+function sum(decade, century){
+  console.log('The sum of 10 and 100 is 110');
+}
