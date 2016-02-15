e49ca09aa8719ef840b9964c1b9b5a613f7b0489
recklessmoxie <shellybarkley@ymail.com>
2016-02-15 14:37:28 -0800

    adjusting questions

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
 var pTagOne = document.getElementById('ans-one');
-var pTagTwo = document.getElementsById('ans-two');
-var pTagThree = document.getElementsById('ans-three');
-var pTagFour  = document.getElementsById('ans-four');
+var pTagTwo = document.getElementById('ans-two');
+var pTagThree = document.getElementById('ans-three');
+var pTagFour  = document.getElementById('ans-four');
 
 
 function sum(decade, century){
-  var decadeCentury = decade + century;
-  pTagOne.textContent = console.log('The sum of ' + decade + ' and ' + century + ' is ' + decadeCentury + '.');
-  return decade + century;
+  var result = decade + century;
+  pTagOne.textContent =('The sum of ' + decade + ' and ' + century + ' is ' + result + '.');
+  return result;
 
 };
 
 function multiply(cloud, moon){
   var cloudMoon = cloud * moon;
-  pTagTwo.textContent = console.log('The product of ' + cloud + ' and ' + moon + ' is ' + cloudMoon + '.')
+  pTagTwo.textContent =('The product of ' + cloud + ' and ' + moon + ' is ' + cloudMoon + '.')
   return cloud * moon;
 };
 
@@ -22,18 +22,18 @@ function sumAndMultiply(decade, century, moon){
   var time = decade + century + moon;
   var space = decade * century * moon;
   var earth = [time, space];
-  pTagThree.textContent = console.log(decade + ' and ' + century + ' and ' + moon + ' sum to ' + time + '.');
-  pTagThree.textContent = console.log('The numbers ' + decade + ' and ' + century + ' and ' + moon + ' have a product of ' + space + '.');
+  pTagThree.textContent = (decade + ' and ' + century + ' and ' + moon + ' sum to ' + time + '.');
+  pTagThree.textContent = ('The numbers ' + decade + ' and ' + century + ' and ' + moon + ' have a product of ' + space + '.');
   return earth;
 };
 
 function sumArray(thisArray){
   var arraySum = 0;
-  for (i = 0; i < thisArray.length; i++) {
+  for(var i = 0; i < thisArray.length; i++) {
     arraySum += thisArray[i]
   }
-  pTagFour.textContent = console.table(thisArray);
-  pTagFour.textContent = console.log(thisArray.toString() + ' was passed in as an array of numbers ' + ' and ' + arraySum + ' is their sum.');
+  pTagFour.textContent = (thisArray);
+  pTagFour.textContent = (thisArray.toString() + ' was passed in as an array of numbers ' + ' and ' + arraySum + ' is their sum.');
   return arraySum;
 };
 
@@ -42,7 +42,7 @@ function multiplyArray(timesArray){
   for (i = 0; i < timesArray.length; i++) {
     arrayTimes *= timesArray[i]
   }
-  pTagFive.textContent =console.table(timesArray);
-  pTagFive.textContent =console.log('The numbers ' + thisArray.toString() + ' have a product of ' + arrayTimes + '.')
+  pTagFive.textContent = (timesArray);
+  pTagFive.textContent = ('The numbers ' + thisArray.toString() + ' have a product of ' + arrayTimes + '.')
   return arrayTimes;
 };
